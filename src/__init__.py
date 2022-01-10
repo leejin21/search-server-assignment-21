0d4fda4f86884c6de9bfdaa5bb5ac7041b228322
JIN <leejin21c@sookmyung.ac.kr>
2022-01-10 23:33:18 +0900

    feat(docker): flask-migrate 설정 및 도커로 자동화

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_migrate import Migrate
 from .models import db
 from . import config
 import os
@@ -7,9 +7,11 @@ import os
 
 def create_app():
     flask_app = Flask(__name__)
+    flask_app.logger.info('CREATE APP __INIT__')
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
     db.init_app(flask_app)
+    migrate = Migrate(flask_app, db)
     db.create_all()
     return flask_app
--- src/models.py
+++ src/models.py
@@ -8,3 +8,9 @@ class Cats(db.Model):
     name = db.Column(db.String(100))
     price = db.Column(db.Integer)
     breed = db.Column(db.String(100))
+
+class Dogs(db.Model):
+    __tablename__ = 'dogs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    breed = db.Column(db.String(100))
